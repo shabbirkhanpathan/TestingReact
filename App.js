a9618cf0870967431ab051f6438764062b704278
shabbirkhanpathan <shabbirkhanpathan.soboft@gmail.com>
2023-03-01 17:56:08 +0530

    getValue

--- App.js
+++ App.js
@@ -1,63 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { View, FlatList, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {View, Text, TextInput, Button} from "react-native";
 
-function App() {
-  const [users, setUsers] = useState([]);
-
-  const fetchUsers = async () => {
-    const res = await fetch("https://reqres.in/api/users/");
-    const json = await res.json();
-    setUsers(json.data);
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const renderItem = ({ item }) => (
-    <View style={styles.userContainer}>
-      <Image style={styles.userImage} source={{ uri: item.avatar }} />
-      <View style={styles.userInfoContainer}>
-        <Text style={styles.userName}>{item.first_name} {item.last_name}</Text>
-        <Text style={styles.userEmail}>{item.email}</Text>
-      </View>
+function App ()  {
+  const [name, setName] = useState();
+  return(
+    <View style={{marginHorizontal:20}}>
+      <Text> Hello </Text> 
+      <Text>Type Your Name: {name} </Text>
+     <TextInput style={{borderWidth:2, borderColor:'blue'}} placeholder="name" value={name} onChangeText={(text)=>setName(text)}/>
+     <Button title="Clear Input" onPress={()=>setName()}/>
     </View>
-  );
-
-  return (
-    <View>
-      <FlatList
-        data={users}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  userContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
-  },
-  userImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    marginRight: 10
-  },
-  userInfoContainer: {
-    flex: 1
-  },
-  userName: {
-    fontWeight: 'bold'
-  },
-  userEmail: {
-    color: '#666'
-  }
-});
+  )}
 
 export default App;
